8d0b61419c011d3e589ceecc9aab997025c95b45
Kevin Barnes <kbarnes3@gmail.com>
2014-10-03 17:38:04 -0700

    Adding lineBrush class

--- impressionist/impressionistDoc.cpp
+++ impressionist/impressionistDoc.cpp
@@ -17,6 +17,7 @@
 
 // Include individual brush headers here.
 #include "pointBrush.h"
+#include "lineBrush.h"
 
 
 #define DESTROY(p)	{  if ((p)!=NULL) {delete [] p; p=NULL; } }
@@ -40,7 +41,7 @@ ImpressionistDoc::ImpressionistDoc()
 
 	// Note: You should implement these 5 brushes.  They are set the same (PointBrush) for now
 	ImpBrush::c_pBrushes[BRUSH_LINES]				
-		= new PointBrush( this, "Lines" );
+		= new LineBrush(this, "Lines");
 	ImpBrush::c_pBrushes[BRUSH_CIRCLES]				
 		= new PointBrush( this, "Circles" );
 	ImpBrush::c_pBrushes[BRUSH_SCATTERED_POINTS]	
